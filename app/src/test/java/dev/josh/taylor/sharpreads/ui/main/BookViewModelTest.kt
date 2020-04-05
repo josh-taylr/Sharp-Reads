de7c256f8deca144e180cbb07d1f5c46cba57ca1
Josh Taylor <joshua.taylor.dev@gmail.com>
2020-04-05 15:51:37 +1200

    Expand unit testing of book view model

--- app/src/test/java/dev/josh/taylor/sharpreads/ui/main/BookViewModelTest.kt
+++ app/src/test/java/dev/josh/taylor/sharpreads/ui/main/BookViewModelTest.kt
@@ -6,11 +6,13 @@ import dev.josh.taylor.goodreadsapi.Book
 import dev.josh.taylor.goodreadsapi.GoodReadsService
 import dev.josh.taylor.goodreadsapi.Search
 import dev.josh.taylor.goodreadsapi.Work
+import dev.josh.taylor.sharpreads.architecture.Event
 import dev.josh.taylor.sharpreads.test.MainCoroutineRule
 import dev.josh.taylor.sharpreads.test.getOrAwaitValue
 import dev.josh.taylor.sharpreads.test.runBlocking
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -39,9 +41,35 @@ class BookViewModelTest {
     lateinit var booksErrorObserver: Observer<Boolean>
 
     @Mock
+    lateinit var bookObserver: Observer<Book>
+
+    @Mock
+    lateinit var navigateToBookObserver: Observer<Event<Book>>
+
+    @Mock
     lateinit var goodReadsService: GoodReadsService
 
     private lateinit var viewModel: BookViewModel
+
+    private val bookId = 101
+    private val book = Book(bookId, "Fooled by Randomness")
+    private val search = Search(
+        start = 0,
+        end = 1,
+        totalCount = 1,
+        results = mutableListOf(
+            Work(
+                id = 1,
+                book = book
+            )
+        )
+    )
+    private val emptySearch = Search(
+        start = 0,
+        end = 0,
+        totalCount = 0,
+        results = mutableListOf()
+    )
 
     @Before
     fun setup() {
@@ -51,18 +79,6 @@ class BookViewModelTest {
     @Test
     fun fetchBooks() = mainCoroutineRule.runBlocking {
         // given
-        val book = Book(101, "Fooled by Randomness")
-        val search = Search(
-            start = 0,
-            end = 1,
-            totalCount = 1,
-            results = mutableListOf(
-                Work(
-                    id = 1,
-                    book = book
-                )
-            )
-        )
         whenever(goodReadsService.search(anyString())).thenReturn(search)
 
         // when books are observed
@@ -70,6 +86,18 @@ class BookViewModelTest {
 
         // then
         assertEquals(listOf(book), books)
+    }
+
+    @Test
+    fun fetchEmptyBooks() = mainCoroutineRule.runBlocking {
+        // given
+        whenever(goodReadsService.search(anyString())).thenReturn(emptySearch)
+
+        // when books are observed
+        val books = viewModel.books.getOrAwaitValue()
+
+        // then
+        assertEquals(emptyList<Book>(), books)
     }
 
     @Test
@@ -85,5 +113,61 @@ class BookViewModelTest {
 
         // then
         verify(booksErrorObserver).onChanged(true)
+    }
+
+    @Test
+    fun loadingState() = mainCoroutineRule.runBlocking {
+        // given
+        whenever(goodReadsService.search(anyString())).thenReturn(search)
+
+        // when
+        val isLoading = viewModel.isLoading.getOrAwaitValue()
+
+        // then
+        assertFalse(isLoading)
+    }
+
+    @Test
+    fun navigateToBook() = mainCoroutineRule.runBlocking {
+        // given
+        whenever(goodReadsService.search(anyString())).thenReturn(search)
+        viewModel.navigateToBook.observeForever(navigateToBookObserver)
+        viewModel.books.getOrAwaitValue()
+
+        // when
+        viewModel.bookItemClicked(0)
+
+        // then
+        verify(navigateToBookObserver).onChanged(Event(book))
+    }
+
+    @Test
+    fun setBook() = mainCoroutineRule.runBlocking {
+        // given
+        whenever(goodReadsService.search(anyString())).thenReturn(search)
+        viewModel.book.observeForever(bookObserver)
+        viewModel.books.getOrAwaitValue()
+
+        // when
+        viewModel.setBook(bookId)
+
+        // then
+        verify(bookObserver).onChanged(book)
+    }
+
+    @Test
+    fun closeBook() = mainCoroutineRule.runBlocking {
+        // given
+
+        whenever(goodReadsService.search(anyString())).thenReturn(search)
+        viewModel.book.observeForever(bookObserver)
+        viewModel.books.getOrAwaitValue()
+        viewModel.setBook(bookId)
+
+        // when
+        viewModel.closeBook()
+
+        // then
+        verify(bookObserver).onChanged(null)
     }
 }
